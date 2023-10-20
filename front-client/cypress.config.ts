4e93b0a7558731e5fcabfe3eb5510d23f190b600
RSelwa <selwa.raphael@gmail.com>
2023-10-20 15:24:10 +0200

    ✅ setup cypress

---
+++ front-client/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  video: false,
+  chromeWebSecurity: false,
+  requestTimeout: 10_000,
+  taskTimeout: 10_000,
+  execTimeout: 10_000,
+  responseTimeout: 10_000,
+  defaultCommandTimeout: 10_000,
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    }
+  }
+})
