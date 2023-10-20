13ed757f1655cf0f599fabf83a413799d7ce7dc7
RSelwa <selwa.raphael@gmail.com>
2023-10-20 10:48:47 +0200

    ✨ Feat/redux

--- front-client/pages/index.tsx
+++ front-client/pages/index.tsx
@@ -1,5 +1,10 @@
+import { useAppSelector } from "@/redux/hook";
+import { selectEmail } from "@/redux/user/userSlice";
 
 export default function Home() {
+  const userEmail = useAppSelector(selectEmail)
+  console.log(userEmail);
+  
   return (
     <main>test </main>
   );
