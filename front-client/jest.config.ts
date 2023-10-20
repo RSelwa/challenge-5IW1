9dfbc1a9b1ee9a5d73431b91d94d3a8a0b96b949
RSelwa <selwa.raphael@gmail.com>
2023-10-20 14:02:10 +0200

    ➕ add jest

---
+++ front-client/jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest"
+
+const config: Config = {
+  verbose: true,
+  moduleNameMapper: {
+    // Aliases
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "<rootDir>/cypress/",
+  ],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  transform: {
+    "^.+\\.(t|j)sx?$": "ts-jest"
+  },
+}
+
+export default config
