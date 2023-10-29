197a86e00566f7661ff9aa2709d608c172bc3599
Raphael SELWA <raphalex01@gmail.com>
2023-10-29 11:05:21 +0100

    📦 add classnames and cva

--- monorepo/utils/index.ts
+++ monorepo/utils/index.ts
@@ -1,4 +1,12 @@
+import type { ClassArray } from "clsx"
+import clsx from "clsx"
+// eslint-disable-next-line no-restricted-imports
+import { twMerge } from "tailwind-merge"
+
 export const logHello = () => {
-  console.log("hello");
-};
-export const sum = (a: number, b: number) => a + b;
+  console.log("hello")
+}
+export const sum = (a: number, b: number) => a + b
+export const cn = (...inputs: ClassArray) => {
+  return twMerge(clsx(inputs))
+}
