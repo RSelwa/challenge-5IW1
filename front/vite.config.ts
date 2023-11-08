4693b59e83a4402dcf1ee75a7809731cd5e96bf3
Stabien <bastien.piedallu@gmail.com>
2023-11-08 16:10:04 +0100

    feat(ops): update docker

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
   },
   server: {
-    port: 3000
+    port: 3000,
+    watch: {
+      usePolling: true
+    }
   }
 })
