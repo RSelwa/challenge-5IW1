1a2620c1275df84afa0a167a54b87239d6898e1f
RSelwa <selwa.raphael@gmail.com>
2023-11-30 23:00:19 +0100

    🚧 wip layout admin

--- front/src/components/Layout/AdminLayout.tsx
+++ front/src/components/Layout/AdminLayout.tsx
@@ -1,13 +1,23 @@
 import React from "react"
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+import { Link } from "react-router-dom"
 import LogoutButton from "@/components/Layout/logoutButton"
+import { dropdownMenuSideOffset } from "@/constants"
 
 const AdminLayout = () => {
   return (
-    <DropdownMenu.Content className="DropdownMenuContent text-red-500">
+    <DropdownMenu.Content
+      sideOffset={dropdownMenuSideOffset}
+      className="DropdownMenuContent text-red-500"
+    >
       <DropdownMenu.Item className="DropdownMenuItem">
         Admin layout
       </DropdownMenu.Item>
+      <DropdownMenu.Separator className="DropdownMenuSeparator" />
+      <DropdownMenu.Item className="DropdownMenuItem">
+        <Link to="/admin/users">users </Link>
+      </DropdownMenu.Item>
+      <DropdownMenu.Separator className="DropdownMenuSeparator" />
       <DropdownMenu.Item className="DropdownMenuItem">
         <LogoutButton />
       </DropdownMenu.Item>
---
+++ front/src/constants/index.ts
@@ -0,0 +1,1 @@
+export const dropdownMenuSideOffset = 16
