da2486861ab976cb5aa7e54d5ed240e8f09cc70d
RSelwa <selwa.raphael@gmail.com>
2023-12-05 18:39:57 +0100

    🐛 Fix signin

--- front/src/lib/auth.ts
+++ front/src/lib/auth.ts
@@ -1,11 +1,9 @@
 import type { LoginFormData, SigninUserFormData } from "@/types/formData"
+import { USER_API_ROUTES } from "@/constants/db"
 import { requestOptions } from "@/utils/db"
 import { removeLocalStorage, setLocalStorage } from "@/utils/localStorage"
 
 export const loginUser = async (data: LoginFormData) => {
-  const headers = new Headers()
-  headers.append("Content-Type", "application/json")
-
   const response = await fetch(
     `${import.meta.env.VITE_API_URL}/auth`,
     requestOptions({ method: "POST", data: data })
@@ -17,17 +15,12 @@ export const loginUser = async (data: LoginFormData) => {
 }
 
 export const signInUser = async (data: SigninUserFormData) => {
-  const headers = new Headers()
-  headers.append("Content-Type", "application/json")
-
   const response = await fetch(
-    `${import.meta.env.VITE_API_URL}/api/users`,
-    requestOptions({ method: "POST", data: JSON.stringify(data) })
+    `${import.meta.env.VITE_API_URL}${USER_API_ROUTES}`,
+    requestOptions({ method: "POST", data: data })
   )
   if (!response.ok) throw new Error("Something went wrong")
-
-  const responseData = await response.json()
-  console.log(responseData)
+  await loginUser({ email: data.email, password: data.plainPassword })
 }
 
 export const logout = () => {
