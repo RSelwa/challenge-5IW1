3b69974a213b1d7f459351bc8a9aaf0aac1f34a0
RSelwa <selwa.raphael@gmail.com>
2023-12-06 12:33:34 +0100

    👷 Fix ci build

--- front/src/constants/mocks/reservations.ts
+++
@@ -1,8 +0,0 @@
-import type { ReservationWithId } from "@/types/withId"
-
-export const reservationMock: ReservationWithId = {
-  clientId: "clientId",
-  date: new Date(),
-  id: "reservationId",
-  prestationId: "prestationId"
-}
--- front/src/constants/mocks/users.ts
+++
@@ -1,8 +0,0 @@
-import type { UsersWithId } from "@/types/withId"
-
-export const clientMock: UsersWithId = {
-  id: "clientId",
-  email: "mock1@yopmail.com",
-  firstname: "firstname",
-  lastname: "lastname"
-}
