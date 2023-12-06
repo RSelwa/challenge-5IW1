043c7a1a96550bf8c22edded5f3d015b575e49ff
RSelwa <selwa.raphael@gmail.com>
2023-12-06 12:33:34 +0100

    👷 Fix ci build

--- front/src/constants/mocks/clinic.ts
+++
@@ -1,9 +0,0 @@
-import type { ClinicWithId } from "@/types/withId"
-
-export const clinicMock: ClinicWithId = {
-  id: "clinicId",
-  location: "",
-  name: "clinicTop",
-  organizationId: "orgId",
-  practitionerIds: ["1", "2", "3"]
-}
--- front/src/constants/mocks/practitioner.ts
+++
@@ -1,8 +0,0 @@
-import type { PractitionerWithId } from "@/types/withId"
-
-export const practitionerMock: PractitionerWithId = {
-  id: "1",
-  email: "practiotioner@yopmail.com",
-  clinicId: "clinicId",
-  planing: "planning"
-}
