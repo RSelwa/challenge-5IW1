ec8c054ef59fb5a7259a9be352fcdd20c259cc18
BASTIEN PIEDALLU <bastien.piedallu@gmail.com>
2023-12-12 00:12:37 +0100

    feat(back): add credentials for employee entity

--- back/src/DataFixtures/EmployeeFixtures.php
+++ back/src/DataFixtures/EmployeeFixtures.php
@@ -4,7 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Employee;
 use App\DataFixtures\EstablishmentFixtures;
-use Symfony\Component\Uid\Uuid;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -12,7 +12,12 @@ use Doctrine\Persistence\ObjectManager;
 class EmployeeFixtures extends Fixture implements DependentFixtureInterface
 {
     const EMPLOYEE_REFERENCE = "antoine-dupont";
+    private $passwordHasher = null;
     
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
     public function getDependencies()
     {
         return array(
@@ -24,11 +29,12 @@ class EmployeeFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         $employee = new Employee();
-        $employee->setId(Uuid::v4());
         $employee->setFirstname("Antoine");
         $employee->setLastname("Dupont");
         $employee->setEstablishment($this->getReference(EstablishmentFixtures::ESTABLISHMENT_REFERENCE));
         $employee->setService($this->getReference(ServiceFixtures::SERVICE_REFERENCE));
+        $employee->setEmail("employee@test.fr");
+        $employee->setPassword($this->passwordHasher->hashPassword($employee, "test"));
 
         $this->addReference(self::EMPLOYEE_REFERENCE, $employee);
 
