bdd4bc21317b42a94dfa9824b91024e47d2af11e
RSelwa <selwa.raphael@gmail.com>
2024-01-16 00:35:35 +0100

    🚧 planning

--- front/src/pages/planning/index.tsx
+++ front/src/pages/planning/index.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons"
+import { Fragment, useEffect, useState } from "react"
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
 import { Button } from "@radix-ui/themes"
 import { dayInSeconds, weekInSeconds } from "@/constants/date"
 import {
@@ -9,11 +9,12 @@ import {
   getDateFromWeek,
   getInitialDay
 } from "@/utils/date"
+import { cn } from "@/utils"
 
 const Planning = () => {
   const [currentDate, setCurrentDate] = useState<Date>(new Date())
   const [weekDays, setWeekDays] = useState<Date[]>([])
-
+  const scaleButton = 24
   const changeWeek = (nextWeek: boolean) =>
     setCurrentDate(
       (prevSate) =>
@@ -36,33 +37,55 @@ const Planning = () => {
         )
       ])
   }
+  const cantChangePreviousWeek = (): boolean => {
+    const currentTime = new Date().getTime()
+    const begginingOfTheWeek = currentDate.getTime()
+    const endOfTheWeek = currentDate.getTime() + weekInSeconds * 1000
 
+    if (begginingOfTheWeek < currentTime && currentTime < endOfTheWeek)
+      return true
+    return false
+  }
   useEffect(() => {
     loadDatesWeek()
   }, [currentDate])
 
   return (
-    <div>
+    <Fragment>
       <div>{currentDate.toUTCString()}</div>
-      <div>
-        <Button onClick={() => changeWeek(false)}>
-          <ArrowLeftIcon />
+      <div className="flex w-fit gap-4 rounded bg-cyan-50 p-2">
+        <Button
+          variant="ghost"
+          onClick={() => changeWeek(false)}
+          disabled={cantChangePreviousWeek()}
+          className={cn(
+            cantChangePreviousWeek() ? "text-gray-500" : "text-cyan-500"
+          )}
+        >
+          <ChevronLeftIcon height={scaleButton} width={scaleButton} />
         </Button>
-        <Button onClick={() => changeWeek(true)}>
-          <ArrowRightIcon />
-        </Button>
-      </div>
-      <div className="flex gap-2">
-        {weekDays.map((e, i) => (
-          <div key={i} className="flex flex-col gap-2 bg-cyan-100 p-4 rounded">
-            <div className="flex flex-col items-center">
-              <span className="font-bold text-lg">{dayOfWeek(e, true)}</span>
-              <span>{dateToString(e)}</span>
+        <div className="flex gap-2">
+          {weekDays.map((e, i) => (
+            <div
+              key={i}
+              className="flex flex-col gap-2 rounded bg-cyan-100 p-4"
+            >
+              <div className="flex flex-col items-center">
+                <span className="text-lg font-bold">{dayOfWeek(e, true)}</span>
+                <span>{dateToString(e)}</span>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <Button
+          variant="ghost"
+          onClick={() => changeWeek(true)}
+          className={cn("text-cyan-500")}
+        >
+          <ChevronRightIcon height={scaleButton} width={scaleButton} />
+        </Button>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
