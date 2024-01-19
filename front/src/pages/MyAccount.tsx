fd5eeec759e598b66e0961a8d0d45f65f69d4a71
cbillien <billienclement@hotmail.com>
2024-01-19 11:49:28 +0100

    userInfosForm

--- front/src/pages/MyAccount.tsx
+++ front/src/pages/MyAccount.tsx
@@ -6,7 +6,7 @@ const MyAccount = () => {
   return (
     <div className="h-[calc(100vh-64px)] bg-blue-100 p-8">
       <div className="mx-auto w-10/12 rounded bg-white p-2 ">
-        <h1 className="mb-6 mt-2 text-center text-xl font-black text-neutral-900">
+        <h1 className="mb-6 mt-2 text-left text-xl font-black text-neutral-900">
           Mes informations 
         </h1>
         <UserInfosForm />
