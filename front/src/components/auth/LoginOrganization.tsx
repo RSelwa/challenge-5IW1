ad14ab543a708e5957a8158011c71dbfc2e67be6
cbillien <billienclement@hotmail.com>
2024-01-25 16:30:24 +0100

    signinOrga front

---
+++ front/src/components/auth/LoginOrganization.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react"
+import { Button } from "@radix-ui/themes"
+import { useForm } from "react-hook-form"
+import toast from "react-hot-toast"
+import { useNavigate, useParams } from "react-router-dom"
+// import type { DbTableLogin } from "@/types/auth"
+import type { LoginFormData } from "@/types/formData"
+import { dbTableLogin } from "@/constants/auth"
+//import { loginOrganization } from "@/lib/auth"
+import { handleSubmitTextDb } from "@/utils/db"
+
+const LoginOrga = () => {
+  const navigate = useNavigate()
+  const { db } = useParams<{ db: string }>()
+  const { handleSubmit, register } = useForm<LoginFormData>()
+
+//   useEffect(() => {
+//     if (!dbTableLogin.includes(db as DbTableLogin)) navigate("/login")
+//   }, [])
+
+  return (
+    <div className="w-10/11 mx-auto md:w-1/2">
+      <form
+        // onSubmit={handleSubmit((data) =>
+        //   toast.promise(loginOrganization(data), {
+        //     success: () => {
+        //       navigate("/")
+        //       return "logged"
+        //     },
+        //     error: (error) => {
+        //       console.trace(error)
+        //       return "Error while login"
+        //     },
+        //     loading: "loading"
+        //   })
+        // )}
+        className="flex flex-col gap-2"
+      >
+        <input placeholder="Email" type="email" {...register("email")} />
+        <input
+          placeholder="Password"
+          type="password"
+          {...register("password")}
+        />
+        <Button
+          className="bg-amber-500 font-bold text-neutral-800"
+          type="submit"
+        >
+          {handleSubmitTextDb(db || "")}
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginOrga
--- front/src/pages/auth/SigninOrganisation.tsx
+++ front/src/pages/auth/SigninOrganisation.tsx
@@ -1,15 +1,54 @@
 import React from "react"
+import * as Accordion from "@radix-ui/react-accordion"
 import SigninOrganizationForm from "@/components/auth/Signin/signinOrganizationForm"
+import LoginWithPassword from "@/components/auth/Login"
+import LoginOrga from "@/components/auth/LoginOrganization"
 
 const SigninOrganization = () => {
+  
+
   return (
-    <div className="h-[calc(100vh-64px)] bg-blue-100 p-8">
-      <div className="mx-auto w-10/12 rounded bg-white p-2 ">
-        <h1 className="mb-6 mt-2 text-center text-xl font-black text-neutral-900">
-          S'enregistrer en tant qu'entreprises
-        </h1>
-        <SigninOrganizationForm />
-      </div>
+    <div className="h-[calc(100vh-64px)] bg-blue-50">
+      <Accordion.Root
+        className="flex flex-col gap-8 py-8"
+        type="single"
+        defaultValue="login"
+        collapsible
+      >
+        <Accordion.Item
+          className="mx-auto w-1/2 rounded bg-white p-4"
+          value="login"
+        >
+          <Accordion.Trigger className=" w-full text-center text-lg font-bold">
+            J'ai déjà un compte entreprise Doctolib
+          </Accordion.Trigger>
+          <Accordion.Content className="mt-6">
+            <LoginOrga />
+          </Accordion.Content>
+        </Accordion.Item>
+        <Accordion.Item
+          className="mx-auto w-1/2 rounded bg-white p-4"
+          value="signin"
+        >
+          <Accordion.Trigger className=" w-full text-center text-lg font-bold">
+            Crée son compte entreprise Doctolib ?
+          </Accordion.Trigger>
+          <Accordion.Content className="mt-6">
+            <SigninOrganizationForm />
+          </Accordion.Content>
+        </Accordion.Item>
+        {/* <Accordion.Item
+          className="mx-auto w-1/2 rounded bg-white p-4"
+          value="organisation"
+        >
+          <Accordion.Trigger className=" w-full text-center text-lg font-bold">
+            Organization
+          </Accordion.Trigger>
+          <Accordion.Content className="mt-6">
+            <SigninEntreprises />
+          </Accordion.Content>
+        </Accordion.Item> */}
+      </Accordion.Root>
     </div>
   )
 }
