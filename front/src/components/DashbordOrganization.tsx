bfd2536d35d099f010f2dec3d8ba31058a7d8694
cbillien <billienclement@hotmail.com>
2024-02-12 15:14:38 +0100

    graph, couleur et nombres entier

--- front/src/components/DashbordOrganization.tsx
+++ front/src/components/DashbordOrganization.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react"
+import { BarChartIcon } from "@radix-ui/react-icons"
 import {
   ArcElement,
   BarController,
@@ -7,6 +8,7 @@ import {
   Chart,
   ChartDataset,
   Chart as ChartJS,
+  Color,
   Colors,
   Legend,
   LinearScale,
@@ -15,27 +17,77 @@ import {
   PointElement,
   Tooltip
 } from "chart.js"
+import { Translate } from "react-auto-translate"
 import { Bar, Line } from "react-chartjs-2"
-import { ReservationCounts, ReservationData } from "@/types/api/slots"
 import { OrganizationsWithId } from "@/types/withId"
-import { establishmentsHeader } from "@/constants/tableHeaders"
-import { fetchOrganization } from "@/lib/organizations"
-import { parseJwt } from "@/utils/redux"
 
 type ChartData = {
-  // id: number
   labels: string[]
-  datasets: { label?: string; data: number[] }[]
+  datasets: { label?: string; data: number[]; backgroundColor: Color[] }[]
 }
-const DashboardOrga = () => {
-  const { id } = parseJwt(localStorage.getItem("token") || "")
-  const [isLoading, setIsLoading] = useState(false)
-  const [data, setData] = useState<ChartData>({
-    // id: 1,
-    datasets: [{ data: [], label: "slots" }],
+type Props = {
+  establishment: OrganizationsWithId["establishments"]
+}
 
-    labels: ["slots"]
-  })
+const DashboardOrga = ({ establishment }: Props) => {
+  const barColors = [
+    "#FF6384",
+    "#36A2EB",
+    "#FFCE56",
+    "#4BC0C0",
+    "#F77825",
+    "#9966FF",
+    "#00D96F"
+  ]
+
+  const filterData = (
+    establishments: OrganizationsWithId["establishments"]
+  ) => {
+    const dataForBarChart: ChartData = {
+      labels: ["slots"],
+      datasets: [
+        {
+          data: [],
+          backgroundColor: []
+        }
+      ]
+    }
+    establishments.forEach((establishment, i) => {
+      const allOrganisationEmployees = establishment.employees
+      const allServices = allOrganisationEmployees
+        .map((employee) => employee.services)
+        .flat()
+
+      const allSlots = allServices.map((service) => service.slots).flat()
+
+      dataForBarChart.labels[i] = establishment.name
+      dataForBarChart.datasets[0].data.push(allSlots.length)
+      dataForBarChart.datasets[0].label = "slots"
+      dataForBarChart.datasets[0].backgroundColor.push(
+        barColors[i % barColors.length]
+      )
+    })
+
+    return dataForBarChart
+  }
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          //  fonction de rappel pour transformer les ticks en nombres entiers
+          callback: function (value) {
+            if (Number.isInteger(value)) {
+              return value
+            }
+          }
+        }
+      }
+    }
+  }
+
+  const [data, setData] = useState<ChartData>(filterData(establishment))
   const [organisation, setOrganisation] = useState<OrganizationsWithId>()
   ChartJS.register(
     ArcElement,
@@ -51,56 +103,40 @@ const DashboardOrga = () => {
     Colors
   )
 
-  const filterData = (
-    establishments: OrganizationsWithId["establishments"]
-  ) => {
-    const dataForBarChart: ChartData = {
-      labels: ["slots"],
-      datasets: [
-        {
-          data: []
-        }
-      ]
-    }
-    establishments.forEach((establishment, i) => {
-      const allOrganisationEmployees = establishment.employees
-      const allServices = allOrganisationEmployees
-        .map((employee) => employee.services)
-        .flat()
+  // const fechMyOrga = async () => {
+  //   setIsLoading(true)
+  //   try {
+  //     const orga = await fetchOrganization(id)
+  //     if (!orga || !orga?.establishments) return
+  //     setOrganisation(orga)
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  //   setIsLoading(false)
+  // }
 
-      const allSlots = allServices.map((service) => service.slots).flat()
+  // useEffect(() => {
+  //   fechMyOrga()
+  // }, [])
 
-      dataForBarChart.labels[i] = establishment.name
-      dataForBarChart.datasets[0].data.push(allSlots.length)
-    })
-
-    setData(dataForBarChart)
-  }
-
-  const fechMyOrga = async () => {
-    setIsLoading(true)
-    try {
-      const orga = await fetchOrganization(id)
-      if (!orga || !orga?.establishments) return
-      setOrganisation(orga)
-    } catch (error) {
-      console.error(error)
-    }
-    setIsLoading(false)
-  }
-
-  useEffect(() => {
-    fechMyOrga()
-  }, [])
-
-  useEffect(() => {
-    if (organisation && organisation.establishments)
-      filterData(organisation.establishments)
-  }, [organisation])
+  // useEffect(() => {
+  //   if (organisation && organisation.establishments)
+  //     filterData(organisation.establishments)
+  // }, [organisation])
 
   return (
-    <div>
-      <Bar datasetIdKey="id" data={data} />
+    <div className=" flex flex-col ">
+      <div className="  flex items-center">
+        <BarChartIcon />
+
+        <h1 className=" text-lg font-bold">
+          Total des réservations par établissements
+        </h1>
+      </div>
+
+      <div className="w-3/5 min-w-80">
+        <Bar datasetIdKey="id" data={data} options={options} />
+      </div>
     </div>
   )
 }
